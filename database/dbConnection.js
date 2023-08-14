c9f66c2dcc5d683c50f6084b5f8bb6facfb63884
Eduardo <eduardokunmixyeah@gmail.com>
2023-08-13 23:27:20 -0400

    responsive login

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASS,
-    database: process.env.DATABASE,
-    port: process.env.PORT,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mercadopereferico',
+    port: 3306,
 })
 
 connection.connect((error) => {
